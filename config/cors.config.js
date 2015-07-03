8d828c14c06b0ee4d0682c1f5b92f2e0c2b00fcd
andela-eashikodi <emeka.ashikodi@andela.co>
2015-07-03 12:39:42 +0100

    improve app structure

---
+++ config/cors.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  corsOptions: {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'X-Requested-With', 'Authorization']
+  }
+};
