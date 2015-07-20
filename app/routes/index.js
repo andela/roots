ec69fa7a7ae656a8a70f41ad624fe537dd92f6f5
andela-eashikodi <emeka.ashikodi@andela.co>
2015-07-21 00:10:28 +0100

    remove unneccessary route

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,20 +6,9 @@ var config = require('../../config/cors.config');
 module.exports = function(app) {
   var UserCtrl = require('../controllers/user.controller');
 
-  router.route('/')
-    .get(function(req, res) {
-      res.json({success: true, text:'Welcome to World Tree Inc'});
-    });
-
   router.route('/users')
     .post(UserCtrl.userSignup);
 
   app.use(cors(config.corsOptions));
-
   app.use('', router);
-
-  app.use(function(req, res, next) {
-    res.status(404).json({error: "The path does not exists"});
-    next();
-  });
 };
