b7746dd90d5a133b76ac3137010b1e632b0443a8
andela-eashikodi <emeka.ashikodi@andela.co>
2015-07-20 16:10:23 +0100

    assign user variable to user model export

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,11 +1,7 @@
 'use strict';
-
-require('../models/user.model');
-
 var mongoose = require('mongoose');
 var config = require('../../config/database.config');
-var User = mongoose.model('User');
-
+var User = require('../models/user.model');
 
 exports.userSignup = function(req, res) {
 
