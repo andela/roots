7739edfd5a99fce60fd4e1910df40b37b9220241
andela-eashikodi <emeka.ashikodi@andela.co>
2015-07-22 17:24:39 +0100

    refactor(backend) require module outside of export

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 var cors = require('cors');
 var config = require('../../config/cors.config');
+var UserCtrl = require('../controllers/user.controller');
 
 module.exports = function(app) {
-  var UserCtrl = require('../controllers/user.controller');
 
   router.route('/users')
     .post(UserCtrl.userSignup);
-
+    
   app.use(cors(config.corsOptions));
   app.use('', router);
 };
