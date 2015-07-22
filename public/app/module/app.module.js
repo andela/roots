f306229526e0f42143bdf999327b9b6abfee35a2
andela-eashikodi <emeka.ashikodi@andela.co>
2015-07-22 17:48:33 +0100

    refactor(frontend) add controller to home route

--- public/app/module/app.module.js
+++ public/app/module/app.module.js
@@ -1,12 +1,17 @@
+'use strict';
+angular.module('eventApp',['ui.router','ngMaterial', 'ngAnimate','ngAria', 'textAngular'])
 
-var App = angular.module('eventApp',['ui.router', 'textAngular']);
-
-App.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
     .state('home',{
       url: '/home',
-      templateUrl: '../app/views/home.view.html'
+      templateUrl: '../app/views/home.view.html',
+      controller: 'homeCtrl'
+    })
+    .state('signup', {
+      url: '/signup',
+      templateUrl: '../app/views/signup.view.html'
     });
 
   $urlRouterProvider.otherwise('/home');
