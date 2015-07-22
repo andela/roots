1048c6fb36997052bbb93e01abb18d479a40c801
andela-eashikodi <emeka.ashikodi@andela.co>
2015-07-22 17:58:10 +0100

    refactor(backend) remove ref to path module

--- server.js
+++ server.js
@@ -2,25 +2,15 @@
 var env = process.env.NODE_ENV || "development";
 var express = require('./config/express');
 var app = express();
-var exp = require('express');
-var path = require('path');
-
 var port = process.env.PORT || 2015;
-
 var config = require('./config/database.config');
 var mongoose = require('mongoose');
 
 mongoose.connect(config[env]);
-app.use(exp.static(path.join(__dirname + '/public')));
-app.get('*', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
 app.listen(port, function(err) {
-  if(err) {
+  if(err){
     console.log(err);
-  } 
-  else {
+  }else{
     console.log('Server started on port: ' + port);
   }
 });
