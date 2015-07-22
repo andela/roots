d8b634cf73fac3d3c5e19af687230883876f2206
andela-eashikodi <emeka.ashikodi@andela.co>
2015-07-22 17:39:15 +0100

    refactor(backend) add route to public folder

--- config/express.js
+++ config/express.js
@@ -2,6 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require("method-override");
+var path = require('path');
+var appDir = path.dirname(require.main.filename);
 
 module.exports = function() {
   var app = express();
@@ -9,6 +11,10 @@ module.exports = function() {
   app.use(bodyParser.json());
   app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
   app.use(methodOverride());
+  app.use(express.static(path.join(appDir + '/public')));
+  app.get('/', function(req, res) {
+    res.sendFile(appDir + '/public/index.html');
+  });
   require('../app/routes/')(app);
   return app;
 };
