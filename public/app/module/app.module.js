748c5e6378abe9f8daf5e737576417cd19677a4b
andela-eashikodi <emeka.ashikodi@andela.co>
2015-07-24 18:47:31 +0100

    refactor(frontend) add controller to stateprovider

--- public/app/module/app.module.js
+++ public/app/module/app.module.js
@@ -8,10 +8,6 @@ angular.module('eventApp',['ui.router','ngMaterial', 'ngAnimate','ngAria', 'text
       url: '/home',
       templateUrl: '../app/views/home.view.html',
       controller: 'homeCtrl'
-    })
-    .state('signup', {
-      url: '/signup',
-      templateUrl: '../app/views/signup.view.html'
     });
 
   $urlRouterProvider.otherwise('/home');
