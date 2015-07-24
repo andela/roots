f8b9ed6b949c14b99f2bad1af54f5904067a4f79
andela-eashikodi <emeka.ashikodi@andela.co>
2015-07-24 17:57:28 +0100

    refactor(frontend) add closeDialog function

--- public/app/controllers/home.controller.js
+++ public/app/controllers/home.controller.js
@@ -4,7 +4,7 @@ angular.module('eventApp')
   .controller('homeCtrl',['$scope', '$mdDialog', '$mdToast', function($scope, $mdDialog, $mdToast) {
 
     $("a[href='#downpage']").click(function() {
-      $("html, body").animate({ scrollTop: $('#event_list').height() }, "slow");
+      $("html, body").animate({ scrollTop: $('#event_list').offset().top}, "slow");
       return false;
     });
 
@@ -18,7 +18,39 @@ angular.module('eventApp')
     };
 
     function UserSignup($scope, $mdDialog) {
+      $scope.closeDialog = function() {
+        $mdDialog.hide();
+      };
+    }
 
+    $scope.login = function(ev) {
+      $mdDialog.show({
+        clickOutsideToClose : true,
+        controller : UserLogin,
+        templateUrl: "app/views/login.view.html",
+        targetEvent: ev
+      });
+    };
+
+    function UserLogin($scope, $mdDialog) {
+      $scope.closeDialog = function() {
+        $mdDialog.hide();
+      };
+    }
+
+    $scope.loginToCreate = function(ev) {
+      $mdDialog.show({
+        clickOutsideToClose : true,
+        controller : ToCreate,
+        templateUrl: "app/views/toCreate.view.html",
+        targetEvent: ev
+      });
+    };
+
+    function ToCreate($scope, $mdDialog) {
+      $scope.closeDialog = function() {
+        $mdDialog.hide();
+      };
     }
 }]);
 
