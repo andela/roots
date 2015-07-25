1babbff2b36050062d973d1403023e14f1379c97
andela-eashikodi <emeka.ashikodi@andela.co>
2015-07-25 21:26:05 +0100

    refactor(backend) modularize users route to a new file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,5 @@
-var express = require('express');
-var router = express.Router();
-var cors = require('cors');
-var config = require('../../config/cors.config');
-var UserCtrl = require('../controllers/user.controller');
+'use strict';
 
 module.exports = function(app) {
- router.route('/users')
-  .post(UserCtrl.userSignup)
-  .get(UserCtrl.getUsers)
-  .delete(UserCtrl.deleteAll);
-
-  app.use(cors(config.corsOptions));
-
-  app.use('/api', router);
+  require('./users.route')(app);
 };
