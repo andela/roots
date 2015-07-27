e2913a26c7a83dac9d1a7dd0e8a9d81daf3b334f
Ore Olarewaju <andela@Andelas-MacBook-Pro-6.local>
2015-07-27 15:55:26 +0100

    test(backend) remove comments

--- spec/unit/user/schemaTest-spec.js
+++ spec/unit/user/schemaTest-spec.js
@@ -13,28 +13,6 @@ describe('User Model', function(done){
     done();
   });
 
-  /*it('should not register invalid entry', function(done){
-    user.firstname = '';
-    user.lastname = '';
-    user.email = '';
-    user.password = '';
-    user.save(function(err){
-      expect(err).not.toBe(null);
-      done();
-    });
-  });
-
-  it('should accept valid entry', function(done){
-    user.firstname = 'adam';
-    user.lastname = 'johnson';
-    user.email = 'adj@yahoo.com';
-    user.password = 'passcode';
-    user.save(function(err){
-      expect(err).toBe(null);
-      done();
-    });
-  });*/
-
 });
 
 
