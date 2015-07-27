82f49f2b02ccd1cc60606fa21d083abad86ee676
Ore Olarewaju <andela@Andelas-MacBook-Pro-6.local>
2015-07-27 11:08:35 +0100

    refactor(backend) edit index.js -require UserController -assign variable to UserController -call function from user.controller.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,25 @@
 'use strict';
 
+var UserController = require('../controllers/user.controller');
+var ctrl = new UserController();
 module.exports = function(app) {
-  require('./users.route')(app);
+ 
+  router.route('/')
+    .get(function(req, res) {
+      return res.send({ success : true, message : 'Mr API'});
+    });
+
+   router.route('/users')
+    .post(ctrl.userSignup)
+    .get(ctrl.getUsers)
+    .delete(ctrl.deleteAll);
+
+  app.use(cors(config.corsOptions));
+
+  app.use('/api', router);
+
+  app.use(function(req, res, next) {
+    res.status(404).json({error: "The path does not exists"});
+    next();
+  });
 };
