7e56647cada9237b36448241b96ee191ad8be27a
Ore Olarewaju <andela@Andelas-MacBook-Pro-6.local>
2015-07-27 15:43:53 +0100

    refactor(backend) modify user.controller.js -modify userSignup function

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -3,20 +3,27 @@ var express = require('express');
 var mongoose = require('mongoose');
 var config = require('../../config/database.config');
 var User = require('../models/user.model');
-// var router = express.Router();
 
 var UserController = function() {
 
 };
 
 UserController.prototype.userSignup = function (req, res) {
+
+  if(!req.body.firstname || !req.body.lastname || !req.body.email || !req.body.password){
+      return res.send(422, { 
+        success : false, 
+        message : 'Check parameters!'
+      });
+  }
+
   User.findOne({email: req.body.email}, function(err, user) {
     if (err) {
       return res.json(err);
     }
     else if (user) {
       res.json({
-        sucess: false,
+        success: false,
         message: 'user email taken'
       });
     } else {
