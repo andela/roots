adf1b8d015a2677448499b7f5a92e072e2bd906b
andela <andela@Opals-MacBook-Pro.local>
2015-07-28 10:49:54 +0100

    remove excess whitespace

--- app/routes/login.route.js
+++ app/routes/login.route.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var LoginCtrl = require('../controllers/logIn.controller');
 var verifyToken = require('../../config/tokenMiddleware');
-//var secret = 'swift';
 
 module.exports = function(router) {
 	router.route('/login')
