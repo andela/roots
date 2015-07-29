c01d2393e92ded30c3fb161f06aa2a0b8d30215c
andela <ufedojo.opaluwa@andela.com>
2015-07-29 14:43:11 +0100

    feat(main app): login functionality - add login.controller.js - add login.route.js - add tokenmiddleware to verify token generated during login - add token verification to routes requiring authorised access - add frontend login code to connect to backend - update package.json with jsonwebtoken - add secret dbconfig file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,12 @@
 'use strict';
 var userRoute = require('./users.route');
-
+var loginRoute = require('./login.route');
 module.exports = function(app) {
   userRoute(app);
+  // loginRoute(app);
+
+  app.use(function(req, res, next) {
+    res.status(404).json({error: "The path does not exists"});
+    next();
+  });
 };
--- app/routes/users.route.js
+++ app/routes/users.route.js
@@ -1,19 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+var verifyToken = require('../../config/tokenMiddleware');
 var UserController = require('../controllers/user.controller');
-
 var ctrl = new UserController();
 module.exports = function(app) {
  
   router.route('/users')
    .post(ctrl.userSignup)
-   .get(ctrl.getUsers)
-   .delete(ctrl.deleteAll);
+   .get(verifyToken,ctrl.getUsers)
+   .delete(verifyToken,ctrl.deleteAll);
+
+  router.route('/authenticate')
+  	.post(ctrl.authenticate)
 
   app.use('/api', router);
-  app.use(function(req, res, next) {
-    res.status(404).json({error: "The path does not exists"});
-    next();
-  });
+  
 };
