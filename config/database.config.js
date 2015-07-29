6f108bb2133e050ef0a65e2d1af745db63fecf2b
andela <ufedojo.opaluwa@andela.com>
2015-07-29 15:44:34 +0100

    -add staging db to database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -4,5 +4,6 @@ module.exports= {
   staging: process.env.MONGOLAB_URI,
   development: 'mongodb://localhost/eventroot',
   production: process.env.MONGOLAB_URI,
+  staging: process.env.MONGOLAB_URI,
   secret: process.env.SESSION_SECRET
 };
