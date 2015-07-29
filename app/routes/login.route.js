ee1449d04256e48f4d61f51e693ddb53229270c4
andela <ufedojo.opaluwa@andela.com>
2015-07-29 14:43:11 +0100

    feat(main app): login functionality - add login.controller.js - add login.route.js - add tokenmiddleware to verify token generated during login - add token verification to routes requiring authorised access - add frontend login code to connect to backend - update package.json with jsonwebtoken - add secret dbconfig file

---
+++ app/routes/login.route.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var LoginController = require('../controllers/login.controller');
+var loginCtrl = new LoginController();
+var verifyToken = require('../../config/tokenMiddleware');
+
+module.exports = function(app) {
+ 
+  router.route('/login')
+   .post(loginCtrl.auth);
+
+  router.route('/users/:user_id')
+   .post(verifyToken,loginCtrl.getCurrentUser)
+   .delete(verifyToken,loginCtrl.deleteCurrentUser);
+
+  app.use('/api', router);
+  app.use(function(req, res, next) {
+    res.status(404).json({error: "The path does not exists"});
+    next();
+  });
+};
