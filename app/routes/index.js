7ebc08373a8d95389a89d4fb31257eca72e3c372
andela-eashikodi <emeka.ashikodi@andela.co>
2015-07-30 12:42:28 +0100

    fix(backend) add middleware to route index file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,14 @@
 'use strict';
 var userRoute = require('./users.route');
+var authRoute = require('./auth.route');
 
-module.exports = function(app) {
+module.exports = function(app, passport) {
   userRoute(app);
+  authRoute(app, passport);
+
+
+  app.use(function(req, res, next) {
+    res.status(404).json({error: "The path does not exists"});
+    next();
+  });
 };
