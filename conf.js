7c64acbbde5663c205ceef65464a9b7020862821
andela-toladele <toyindipol@gmail.com>
2015-07-31 16:33:11 +0100

    Passing user data as token from backend to frontend as req param

--- conf.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec/e2e/**/*.spec.js'],
-  capabilities: {
-    browserName: 'chrome'
-  },
-  baseUrl: 'http://localhost:2015'
-};
