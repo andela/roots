e540f6f3f94f2fa44fb8edda14fd62dd888862d2
andela-toladele <toyindipol@gmail.com>
2015-08-05 15:34:32 +0100

    Removal of authentication keys and renaming of authController function in auth.route.js file

--- config/passport.js
+++ config/passport.js
@@ -12,6 +12,7 @@ module.exports = function() {
 
   //google strategy
   passport.use(new GoogleStrategy({
+
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: '/auth/google/callback',
