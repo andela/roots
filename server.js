5f7285a3d58e34875113f2ec04605681a08b8e95
andela-eashikodi <emeka.ashikodi@andela.co>
2015-08-05 10:34:59 +0100

    fix(backend) remove env variable declaration in code

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 'use strict';
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
 var app = require('./config/express')();
 var port = process.env.PORT || 2015;
 var config = require('./config/config');
