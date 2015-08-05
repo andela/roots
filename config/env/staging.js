75c87cfefb6e0d675b44be395302c52d0347e70f
andela-eashikodi <emeka.ashikodi@andela.co>
2015-08-05 11:08:47 +0100

    fix(backend) remove env variable declaration in code

--- config/env/staging.js
+++ config/env/staging.js
@@ -4,8 +4,8 @@ module.exports = {
   db: process.env.MONGOLAB_URI,
   secret: process.env.SESSION_SECRET,
   google : {
-    clientID: '951983031173-vdj0a4k0q3apqtk57v873i0gj0n6o3l9.apps.googleusercontent.com',
-    clientSecret: '25cs-KXxdpMIEyWu85CTxGQF'
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
   },
   corsOptions: {
     origin: '*',
