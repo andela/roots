320455012c8577dfb8a91b1e4968b139e86e86e1
Ore Olarewaju <andela@Andelas-MacBook-Pro-6.local>
2015-08-06 10:47:10 +0100

    refactor(backend) #48 twitter authentication - add twitterAuthCallback function - add twitter callback route - use express-session - npm install express-session and passport-twitter

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -14,6 +14,26 @@ authController.prototype.authCallback = function(strategy) {
         res.redirect('/#/home');
       }
       else {
+
+        var token = jwt.sign(user, config.secret, {
+            expiresInMinutes: 1440 //24hr expiration
+        });
+        res.redirect('/#/home?token=' + token);
+      }
+    })(req, res, next);
+  };
+};
+
+authController.prototype.twitterAuthCallback = function(strategy) {
+  return function(req, res, next) {
+    passport.authenticate(strategy, function(err, user) {
+      if(err) {
+        return next(err);
+      }
+      if(!user){
+        res.redirect('/#/home');
+      }
+      else {
         var token = jwt.sign(user, config.secret, {
             expiresInMinutes: 1440 //24hr expiration
         });
