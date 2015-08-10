77babe7896e830198ae10cdca3ec3eebf0102570
andela-eashikodi <emeka.ashikodi@andela.co>
2015-08-10 14:45:40 +0100

    feat(frontend) add controllers for resetpassword view, new twitter sign up view

---
+++ public/app/controllers/resetpass.controller.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('eventApp')
+  .controller('resetPasswordCtrl', ['$scope', 'UserService', '$stateParams', '$location', '$timeout', function($scope, UserService, $stateParams, $location, $timeout) {
+
+    $scope.token = $stateParams.token;
+    $scope.savePassword = function(token, password) {
+      if ($scope.password1 === $scope.password2) {
+        UserService.resetPassword(token, {'password': password})
+          .success(function(data) {
+            $scope.successMessage = true;
+            $scope.passwordMismatch = false;
+            $scope.passReset = true;
+            $timeout(function(){$location.url('/home');}, 3000);
+          });
+      } else if ($scope.password2 && $scope.password1) {
+        $scope.passwordMismatch = true;
+      }
+    };
+  }]);
