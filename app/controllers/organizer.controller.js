6a583bb0e714e7d529c0019ecec430a612ca0263
andela-toladele <toyindipol@gmail.com>
2015-08-13 18:05:37 +0100

    Creating controller for Organizer profile

--- app/controllers/organizer.controller.js
+++ app/controllers/organizer.controller.js
@@ -32,10 +32,8 @@ OrganizerController.prototype.createProfile = function(req, res) {
         newOrgProfile.name = req.body.organName;
         var newStaff = req.body.staff;
         var validatedStaff = [];
-        var errLog = "";
 
         newOrgProfile.save(function(err, orgProfile) {
-
 
           if (err) {
             if (err.code == 11000)
@@ -48,73 +46,12 @@ OrganizerController.prototype.createProfile = function(req, res) {
 
           if (newStaff) {
 
-            newStaff.forEach(function(person) {
-
-              User.find({
-                email: person.email
-              }, function(err, personData) {
-
-                if (personData) {
-
-                  Organizer.findOne({
-                    _id: orgProfile._id,
-                    staff.manager_ref: personData._id
-                  }, function(err, duplicate) {
-
-                    if (!err && !duplicate) {
-                      validatedStaff.push({
-                        manager_ref: personData._id,
-                        role: person.role
-                      });
-                    }
-
-                  });
-                }
-              });
-            });
-
-
-            //Add validated users as staff
-            orgProfile.staff = validatedStaff;
-
-            orgProfile.save(function(err, saved) {
-              if (err) {
-                errLog = "Unable to add users as staff";
-              } else {
-
-
-                //Send notification mail to all added staff
-                Organizer.findOne({
-                  _id: orgProfile._id
-                }).populate('manager_ref').exec(function(err, populatedProfile) {
-
-                  if (!err) {
-
-                    var mailOptions = {
-                      to: '',
-                      from: 'World tree ✔ <no-reply@worldtreeinc.com>',
-                      subject: populatedProfile.name + ' added you',
-                      text: populatedProfile.name + ' added you',
-                      html: 'Hello,\n\n' +
-                        'You have been added as staff to <b>' + populatedProfile.name + '</b> event manager.\n'
-                    };
-
-                    populatedProfile.staff.forEach(function(newStaff) {
-
-                      mailOptions.to = newStaff.manager_ref.email;
-                      utils.sendMail(mailOptions);
-
-                    });
-                  }
-
-                });
-              }
-            });
+            OrganizerController.prototype.addTeamMembersStub(orgProfile, newStaff);
 
           };
 
           //Link user profile to the newly created organizer profile
-          user.organizer_ref = profile._id;
+          user.organizer_ref = orgProfile._id;
           user.save(function(err, updatedUser) {
 
             if (err) {
@@ -124,10 +61,7 @@ OrganizerController.prototype.createProfile = function(req, res) {
               });
             }
 
-            return res.json({
-              success: true,
-              message: 'Organizer profile created'
-            });
+            return res.json(orgProfile);
 
           });
 
@@ -157,3 +91,118 @@ OrganizerController.prototype.editProfile = function(req, res) {
     });
   });
 };
+
+OrganizerController.prototype.getProfile = function(req, res) {
+
+  Organizer.findOne({
+    _id: req.params.organizerId
+  }).populate('user_ref manager_ref').exec(function(err, populatedProfile) {
+
+    if (err) {
+      res.send(err);
+    };
+    res.json(populatedProfile);
+
+  });
+}
+
+OrganizerController.prototype.addTeamMembers = function(req, res) {
+
+  if (!req.body.userId || !req.body.newStaff) {
+
+    return res.status(422).send({
+      success: false,
+      message: 'Check parameters!'
+    });
+  }
+
+  Organizer.findOne({
+    user_ref: req.body.userId
+  }, function(err, orgProfile) {
+
+    if (err) {
+      return res.send(err);
+    };
+
+    var updatedProfile = OrganizerController.prototype.addTeamMembersStub(orgProfile, req.body.newStaff);
+
+    if (!updatedProfile)
+      return res.status(422).send({
+        success: false,
+        message: 'Unable to add members'
+      });
+
+    res.json(updatedProfile);
+  });
+}
+
+OrganizerController.prototype.addTeamMembersStub = function(orgProfile, newStaff) {
+
+  var validatedStaff = [];
+
+  newStaff.forEach(function(person) {
+
+    User.find({
+      email: person.email
+    }, function(err, personData) {
+
+      if (personData) {
+
+        Organizer.findOne({
+          _id: orgProfile._id,
+          staff.manager_ref: personData._id
+        }, function(err, duplicate) {
+
+          if (!err && !duplicate) {
+            validatedStaff.push({
+              manager_ref: personData._id,
+              role: person.role
+            });
+          } else {
+            return false;
+          }
+
+
+        });
+      }
+    });
+  });
+
+
+  //Add validated users as staff
+  orgProfile.staff = validatedStaff;
+
+  orgProfile.save(function(err, saved) {
+    if (err)
+      return false;
+
+    //Send notification mail to all added staff
+    Organizer.findOne({
+      _id: orgProfile._id
+    }).populate('manager_ref').exec(function(err, populatedProfile) {
+
+      if (err)
+        return false;
+
+      var mailOptions = {
+        to: '',
+        from: 'World tree ✔ <no-reply@worldtreeinc.com>',
+        subject: populatedProfile.name + ' added you',
+        text: populatedProfile.name + ' added you',
+        html: 'Hello,\n\n' +
+          'You have been added as staff to <b>' + populatedProfile.name + '</b> event manager.\n'
+      };
+
+      populatedProfile.staff.forEach(function(addedStaff) {
+
+        mailOptions.to = addedStaff.manager_ref.email;
+        utils.sendMail(mailOptions);
+
+      });
+      return populatedProfile;
+
+    });
+
+  });
+
+}
