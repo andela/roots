61bdc67c68a945efdc7f2807a6767d77015b45cb
andela-toladele <toyindipol@gmail.com>
2015-08-17 11:46:05 +0100

    Api functions for organizer profile tasks

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -231,6 +231,7 @@ UserController.prototype.deleteCurrentUser = function(req, res) {
         }
       }, function(err) {
         if (err) {
+<<<<<<< HEAD
 
           return res.status(422).send({
             success: false,
@@ -253,6 +254,30 @@ UserController.prototype.deleteCurrentUser = function(req, res) {
 
     } else {
 
+=======
+
+          return res.status(422).send({
+            success: false,
+            message: 'Unable to delete user from other organizer profile.'
+          });
+        }
+
+        User.remove({
+          _id: userId
+        }, function(err, user) {
+          if (err) return res.send(err);
+
+          res.json({
+            message: 'Succesfully deleted'
+          });
+
+        });
+
+      });
+
+    } else {
+
+>>>>>>> Api functions for organizer profile tasks
       return res.status(422).send({
         success: false,
         message: 'User not found in db'
