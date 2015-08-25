328dd060582f1bcb1f4fb0c56b8deb5b0876d4f8
Ore Olarewaju <andela@Andelas-MacBook-Pro-6.local>
2015-08-25 16:11:36 +0100

    feature(frontend)create event - create controller - add function to preview image - add function to change color on preview page

--- public/app/controllers/create.event.controller.js
+++ public/app/controllers/create.event.controller.js
@@ -1,48 +1,38 @@
 angular.module('eventApp')
   .controller('createeventCtrl', function($scope, $rootScope, UserService, $location) {
-	$scope.changeColor = function(elem,elem2) {
-		console.log(elem);
-		$('md-toolbar.bars').css("background-color", elem);
-		$('md-toolbar.bars1').css("background-color", elem2);
-		
-	};
-    
     $scope.createEventPage = function (){
       $location.url('/createevent');
     };
 
-    $("#fileUpload").on('change', function () {
- 
-        if (typeof (FileReader) != "undefined") {
- 
-            var image_holder = $("#image-holder");
-            image_holder.empty();
- 
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                $("<img />", {
-                    "src": e.target.result,
-                    "class": "thumb-image"
-                }).appendTo(image_holder);
- 
-            }
-            image_holder.show();
-            reader.readAsDataURL($(this)[0].files[0]);
-        } else {
-            alert("This browser does not support FileReader.");
-        }
-    });
- 
-  	$rootScope.signupCheck = function() {
-      if(localStorage.getItem('userToken')) {
-        UserService.decodeUser().then(function(res) {
-          $scope.userName = res.data.firstname;
-          $scope.profilePic = res.data.profilePic || "../../assets/img/icons/default-avatar.png";
-          $scope.loggedIn = true;
-        });
+    $rootScope.signupCheck = function() {
+      if (localStorage.getItem('userToken')) {
+        UserService.decodeUser($scope);
       }
-    };
+    }
 
+    $scope.previewFile = function(input, img) {
+      $(input).on('change', function(){
+		var preview = document.querySelector(img);
+		var file    = document.querySelector(input).files[0];
+		var reader  = new FileReader();
+
+		reader.onloadend = function () {
+		  preview.src = reader.result;
+		}
+
+		if (file) {
+		  reader.readAsDataURL(file);
+		} else {
+		  preview.src = "";
+		}
+	  });
+	};
+ 
+    $scope.changeColor = function(elem,elem2) {
+		$('md-toolbar.bars').css("background-color", elem);
+		$('md-toolbar.bars1').css("background-color", elem2);
+	};
+    
 	$scope.event = {
       title: '',
       description: '',
@@ -53,10 +43,12 @@ angular.module('eventApp')
       endDate: '',
       startTime: '',
       endTime: '',
+      bannerImg: '',
       organizerName : '',
       organizerInfo : '',
       organizerPhonenumber1 : '',
       organizerPhonenumber2 : '',
+      logoImg:'',
       organizerTeamMembers : {
         one : {
     	  name : '',
