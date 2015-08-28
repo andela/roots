02e5d486c5ee2349500ca0978cf7cfd25b64845b
andela <ufedojo.opaluwa@andela.com>
2015-08-28 10:08:10 +0100

    refactor(main app): event view / modules - remove modules that are no longer required e.g rangy, textAngular - edit event view

--- public/app/module/app.module.js
+++ public/app/module/app.module.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('eventApp',['ui.router','ngMaterial', 'ngMessages', 'ngResource', 'ngAnimate','ngAria', 'ngWYSIWYG'])
+angular.module('eventApp',['ui.router','ngMaterial', 'ngMessages', 'ngResource', 'ngAnimate','ngAria','summernote'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
