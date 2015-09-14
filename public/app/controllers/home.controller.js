bd29afe4b0a382339ff123201832f4cb83fcca1e
Ore Olarewaju <andela@Andelas-MacBook-Pro-6.local>
2015-09-14 10:47:38 +0100

    refactor(frontend): link browse events view - add more categories - add state for: browse-events                  more-events - link browse-events view

--- public/app/controllers/home.controller.js
+++ public/app/controllers/home.controller.js
@@ -58,6 +58,13 @@ angular.module('eventApp')
 
     };
 
+    $scope.fetchEvents = function() {
+      EventService.getAllEvents().then(function(data){
+        $scope.eventList = data.data;
+      });
+
+    };
+
     function UserLogin($scope, $rootScope, $mdDialog, view) {
       if (view === 'signup') {
         $scope.signupDialog = true;
