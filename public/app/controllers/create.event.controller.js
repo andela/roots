28765f46736a74089bf484c34e48386b7830af2d
andela <ufedojo.opaluwa@andela.com>
2015-09-16 12:44:30 +0100

    resolving merge conflicts

--- public/app/controllers/create.event.controller.js
+++ public/app/controllers/create.event.controller.js
@@ -1,5 +1,13 @@
 angular.module('eventApp')
   .controller('createeventCtrl', function($scope, $rootScope, UserService, $location) {
+
+	$scope.changeColor = function(elem,elem2) {
+		console.log(elem);
+		$('md-toolbar.bars').css("background-color", elem);
+		$('md-toolbar.bars1').css("background-color", elem2);
+
+	};
+
     $scope.createEventPage = function (){
       $location.url('/createevent');
     };
@@ -27,12 +35,45 @@ angular.module('eventApp')
 		}
 	  });
 	};
- 
+
     $scope.changeColor = function(elem,elem2) {
 		$('md-toolbar.bars').css("background-color", elem);
 		$('md-toolbar.bars1').css("background-color", elem2);
 	};
-    
+
+
+    $("#fileUpload").on('change', function () {
+
+        if (typeof (FileReader) != "undefined") {
+
+            var image_holder = $("#image-holder");
+            image_holder.empty();
+
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $("<img />", {
+                    "src": e.target.result,
+                    "class": "thumb-image"
+                }).appendTo(image_holder);
+
+            }
+            image_holder.show();
+            reader.readAsDataURL($(this)[0].files[0]);
+        } else {
+            alert("This browser does not support FileReader.");
+        }
+    });
+
+  	$rootScope.signupCheck = function() {
+      if(localStorage.getItem('userToken')) {
+        UserService.decodeUser().then(function(res) {
+          $scope.userName = res.data.firstname;
+          $scope.profilePic = res.data.profilePic || "../../assets/img/icons/default-avatar.png";
+          $scope.loggedIn = true;
+        });
+      }
+    };
+
 	$scope.event = {
       title: '',
       description: '',
@@ -43,12 +84,14 @@ angular.module('eventApp')
       endDate: '',
       startTime: '',
       endTime: '',
+
       bannerImg: '',
+
       organizerName : '',
       organizerInfo : '',
       organizerPhonenumber1 : '',
       organizerPhonenumber2 : '',
-      logoImg:'',
+
       organizerTeamMembers : {
         one : {
     	  name : '',
