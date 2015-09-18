1a70687e75c98f030e4e3a540e1360e1281e6564
andela-toladele <toyindipol@gmail.com>
2015-09-18 13:05:46 +0100

    Fix(backend) merge conflicts

--- app/models/organizer.model.js
+++ app/models/organizer.model.js
@@ -2,34 +2,43 @@
 
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
-var Schema = mongoose.Schema;
 
 var organizerSchema = new mongoose.Schema({
   user_ref: {
-    type: String
+    type: String,
+    ref: 'User',
+    required: true,
+    index: {
+      unique: true
+    }
   },
   name: {
     type: String,
-    required: true
+    required: true,
+    index: {
+      unique: true
+    }
   },
   about: {
     type: String
   },
   imageUrl: {
     type: String
-  }
-  // staff: [{
-  //   manager_ref: {
-  //     type: String,
-  //     ref: 'User'      
-  //   },
-  //   role: {
-  //     type: String      
-  //   }
-  // }]
+  },
+  staff: [{
+    manager_ref: {
+      type: String,
+      ref: 'User'      
+    },
+    role: {
+      type: String      
+    }
+  }]
 
 }, {
   versionKey: false
 });
 
-mongoose.model('Organizer', organizerSchema);
+var Organizer = mongoose.model('Organizer', organizerSchema);
+
+module.exports = Organizer;
