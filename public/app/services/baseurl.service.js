e393b48c75da1cdcdf760fb45cd76d0a9786a36a
andela-toladele <toyindipol@gmail.com>
2015-10-07 15:23:53 +0100

    Edit base url file

--- public/app/services/baseurl.service.js
+++ public/app/services/baseurl.service.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('eventApp')
-  //.value('baseUrl', 'https://roots-event-manager.herokuapp.com/api/');
-  .value('baseUrl', 'http://localhost:3030/api/');
+  .value('baseUrl', 'https://roots-event-manager.herokuapp.com/api/');
+  //.value('baseUrl', 'http://localhost:3030/api/');
 //.value('baseUrl', 'http://localhost:2015/api/');
