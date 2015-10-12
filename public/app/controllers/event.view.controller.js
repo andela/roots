38dbb86b43b10d95b3922f81aae5c15fca7a2406
andela-toladele <toyindipol@gmail.com>
2015-10-12 15:46:23 +0100

    Fix(Frontend) HTML tags should not be displayed alongside html formatted event description

--- public/app/controllers/event.view.controller.js
+++ public/app/controllers/event.view.controller.js
@@ -1,53 +1,53 @@
 angular.module('eventApp')
-  .controller('eventViewCtrl', ['$scope', '$stateParams', '$location', 'EventService', 'OrganizerService', '$rootScope', '$state','$sce', function($scope, $stateParams, $location, EventService, OrganizerService, $rootScope, $state, $sce) {
+  .controller('eventViewCtrl', ['$scope', '$stateParams', '$location', 'EventService', 'OrganizerService', '$rootScope', '$state', '$sce', function($scope, $stateParams, $location, EventService, OrganizerService, $rootScope, $state, $sce) {
 
-     $scope.services = function(){
+      $scope.services = function(){
 
         EventService.getEvent($stateParams.event_id)
           .success(function(event){
             $scope.event = event;
 
-          $('.md-warn').css('border-color', event.eventTheme.borderColor);
-          $('.md-warn').css('background-color', event.eventTheme.headerColor);
-          $('.md-warn').css('color', event.eventTheme.fontColor);
-          $('.values').css('border-color', event.eventTheme.borderColor);
-          $('.values').css('background-color', event.eventTheme.contentColor);
-          $('.values').css('color', event.eventTheme.fontColor);
-          $scope.address = event.venue.address;
-          $scope.eventInfo = $sce.trustAsHtml($scope.event.description);
-          $scope.canPublish = $rootScope.loggedIn && $rootScope.userId === $scope.event.user_ref._id && !$scope.event.online;
+            $scope.event.description = $sce.trustAsHtml($scope.event.description);
 
-          $scope.canEdit = $rootScope.loggedIn && $rootScope.userId === $scope.event.user_ref._id;
-          
-          if (event.user_ref.organizer_ref) {
+            $('.md-warn').css('background-color', event.eventTheme.headerColor);
+            $('.md-warn').css('color', event.eventTheme.fontColor);
+            $('.values').css('border-color', event.eventTheme.borderColor);
+            $('.values').css('background-color', event.eventTheme.contentColor);
+            $('.values').css('color', event.eventTheme.fontColor);
 
-            OrganizerService.getOrganizer(event.user_ref.organizer_ref)
-              .success(function(organizer) {                
-                $scope.organizer = organizer;
-                $scope.organizer.phoneNumber1 = event.user_ref.phoneNumber1;
-              });
-          }
-        });
+            $scope.canPublish = $rootScope.loggedIn && $rootScope.userId === $scope.event.user_ref._id && !$scope.event.online;
+
+            $scope.canEdit = $rootScope.loggedIn && $rootScope.userId === $scope.event.user_ref._id;
+            
+            if (event.user_ref.organizer_ref) {
+
+              OrganizerService.getOrganizer(event.user_ref.organizer_ref)
+                .success(function(organizer) {                
+                  $scope.organizer = organizer;
+                  $scope.organizer.phoneNumber1 = event.user_ref.phoneNumber1;
+                });
+            }
+          });
       };
 
       $rootScope.hideBtn = false;
 
-    $scope.editEvent = function() {
-      $state.go('user.editEvent', {
-        event_id: $stateParams.event_id
-      });
-      document.body.scrollTop = document.documentElement.scrollTop = 0;
-    }
-
-    $scope.deleteEvent = function(){
-      EventService.deleteEvent($stateParams.event_id);
-      $location.url('/home');
-    };
-
-    $scope.publishEvent = function() {
-      EventService.publishEvent($stateParams.event_id)
-        .success(function(data) {
-          $scope.canPublish = false;          
+      $scope.editEvent = function() {
+        $state.go('user.editEvent', {
+          event_id: $stateParams.event_id
         });
-    }
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+      }
+
+      $scope.deleteEvent = function(){
+        EventService.deleteEvent($stateParams.event_id);
+        $location.url('/home');
+      };
+
+      $scope.publishEvent = function() {
+        EventService.publishEvent($stateParams.event_id)
+          .success(function(data) {
+            $scope.canPublish = false;          
+          });
+      }
   }]);
