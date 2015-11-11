8ec7b10a33b0836819b84408641fb87219036c34
andela-toladele <toyindipol@gmail.com>
2015-11-11 09:31:01 +0100

    Feat(Frontend) Add manage task by task manager feature, view scheduled tasks by volunteer feature

--- public/app/controllers/event.task.controller.js
+++ public/app/controllers/event.task.controller.js
@@ -17,49 +17,90 @@ angular.module('eventApp')
       } else {
         EventService.getEvent($stateParams.event_id)
           .success(function(event) {
-            $scope.event = event.details;
+              $scope.event = event.details;
 
+              $scope.role = event.role;
 
-            TaskService.getAllTasks($stateParams.event_id).success(function(tasks) {
+              if ($scope.isEventOwner()) {
 
-              var user;
+                TaskService.getAllTasks($stateParams.event_id).success(function(tasks) {
 
-              $scope.tasks = tasks.map(function(task) {
+                  var user;
 
-                user = angular.extend({}, task.manager_ref);
-                task.manager_ref = user._id;
-                task.firstname = user.firstname;
-                task.lastname = user.lastname;
-                task.email = user.email;
-                task.startDate = parseDate(task.startDate);
-                task.endDate = parseDate(task.endDate);
+                  $scope.tasks = tasks.map(function(task) {
 
-                return task;
+                    user = angular.extend({}, task.manager_ref);
+                    task.manager_ref = user._id;
+                    task.firstname = user.firstname;
+                    task.lastname = user.lastname;
+                    task.email = user.email;
+                    task.startDate = parseDate(task.startDate);
+                    task.endDate = parseDate(task.endDate);
+
+                    return task;
+                  });
+                });
+
+                OrganizerService.getMyProfile().success(function(res) {
+
+                  var filtered;
+
+                  $scope.staff = res.staff.map(function(member) {
+                    filtered = {};
+                    filtered.memberId = member._id;
+                    filtered.manager_ref = member.manager_ref._id;
+                    filtered.firstname = angular.lowercase(member.manager_ref.firstname);
+                    filtered.lastname = angular.lowercase(member.manager_ref.lastname);
+                    filtered.email = angular.lowercase(member.manager_ref.email);
+                    filtered.profilePic = member.manager_ref.profilePic;
+                    filtered.description = member.role;
+                    return filtered;
+                  });
+
+                  $scope.searchLabel = $scope.staff.length ? "Search team member and assign task..." : "Add team members to organizer profile"
+                });
+              } else if ($scope.isTaskManager()) {
+
+                $scope.volunteers = [];
+                $scope.tasks = [];
+                $scope.schedule = {};
+                $scope.volunteer = {};
+                $scope.pendingVolunteers = [];
+
+                TaskService.getAllUserEventTasks($stateParams.event_id).success(function(tasks) {
+
+                  $scope.tasks = tasks;
+
+                  for (var i = 0; i < tasks.length; i++) {
+
+                    for (var j = 0; j < tasks.volunteers.length; j++) {
+                      var volunteer = {};
+                      volunteer = angular.copy(tasks[i].volunteers[j].volunteer_ref);
+                      volunteer.task = angular.lowercase(tasks[i].description);
+                      volunteer.taskId = tasks[i]._id;
+                      volunteer.user_ref.firstname = angular.lowercase(volunteer.user_ref.firstname);
+                      volunteer.user_ref.lastname = angular.lowercase(volunteer.user_ref.lastname);
+                      $scope.volunteers.push(volunteer);
+                    }
+                  }
+
+                  TaskService.getEventPendingVolunteers($stateParams.event_id).success(function(volunteers) {
+
+                    $scope.pendingVolunteers = volunteers;
+                  });
+                });
+
+                $scope.searchLabel = $scope.staff.length ? "Search team member and assign task..." : "Add team members to organizer profile";
               });
-            });
+          } else if ($scope.isVolunteer()) {
+            $scope.volunteers = [];
+            TaskService.getAllMyEventVolunteers($stateParams.event_id).success(function(volunteers) {
 
+              $scope.volunteers = volunteers;
+            })
 
-
-            OrganizerService.getMyProfile().success(function(res) {
-
-              var filtered;
-
-              $scope.staff = res.staff.map(function(member) {
-                filtered = {};
-                filtered.memberId = member._id;
-                filtered.manager_ref = member.manager_ref._id;
-                filtered.firstname = angular.lowercase(member.manager_ref.firstname);
-                filtered.lastname = angular.lowercase(member.manager_ref.lastname);
-                filtered.email = angular.lowercase(member.manager_ref.email);
-                filtered.profilePic = member.manager_ref.profilePic;
-                filtered.description = member.role;
-                return filtered;
-              });
-
-              $scope.searchLabel = $scope.staff.length ? "Search team member and assign task..." : "Add team members to organizer profile";
-            });
-          });
-      }
+          }
+      };
     };
 
     $rootScope.hideBtn = false;
@@ -127,7 +168,7 @@ angular.module('eventApp')
 
           $window.alert("Task modified!");
 
-        }).error(function(err, status) {          
+        }).error(function(err, status) {
           processError(err, status);
         });
     };
@@ -137,6 +178,215 @@ angular.module('eventApp')
         event_id: $stateParams.event_id
       });
       document.body.scrollTop = document.documentElement.scrollTop = 0;
+    };
+
+    $scope.addVolunteerToTask = function(volunteer) {
+      TaskService.addVolunteerToTask(volunteer.task_ref._id, volunteer._id).success(function(res) {
+
+        volunteer.added = true;
+        volunteer.task_ref = volunteer.task_ref._id;
+
+        for (var i = 0; i < $scope.tasks.length; i++) {
+
+          if ($scope.tasks[i]._id === volunteer.task_ref) {
+
+
+            $scope.tasks[i].push({
+              volunteer_ref: volunteer
+            });
+
+            var volunteerListItem = angular.copy(volunteer);
+
+            volunteerListItem.task = angular.lowercase($scope.tasks[i].description);
+            volunteerListItem.taskId = $scope.tasks[i]._id;
+            volunteerListItem.user_ref.firstname = angular.lowercase(volunteerListItem.user_ref.firstname);
+            volunteerListItem.user_ref.lastname = angular.lowercase(volunteerListItem.user_ref.lastname);
+            $scope.volunteers.push(volunteerListItem);
+
+            var idx = -1;
+            for (var j = 0; j < $scope.pendingVolunteers.length; j++) {
+
+              if ($scope.pendingVolunteers[j]._id === volunteer._id) {
+
+                idx = j;
+                break;
+              }
+            }
+
+            if (idx > -1) {
+
+              $scope.pendingVolunteers.splice(idx, 1);
+            }
+
+            break;
+          }
+        }
+        $window.alert('Volunteer added.');
+      }).error(function(err, status) {
+
+        processError(err, status);
+      });
+    };
+
+
+    $scope.removeVolunteerFromTask = function(taskId, volunteerId, added) {
+      TaskService.removeVolunteerFromTask(taskId, volunteerId).success(function(res) {
+
+        if (added) {
+
+          var idx = -1;
+          for (var i = 0; i < $scope.tasks.length; i++) {
+
+            if ($scope.tasks[i]._id === taskId) {
+
+              for (var j = 0; j < $scope.tasks[i].volunteers.length; j++) {
+                if ($scope.tasks[i].volunteers[j].volunteer_ref._id === volunteerId) {
+                  idx = j;
+                  break;
+                }
+              }
+
+              if (idx > -1) {
+
+                $scope.tasks[i].volunteers.splice(idx, 1);
+              }
+
+              break;
+            }
+          }
+
+          idx = -1;
+
+          for (var i = 0; i < $scope.volunteers.length; i++) {
+
+            if ($scope.volunteers[i]._id === volunteerId) {
+
+              idx = i;
+
+              break;
+            }
+
+          }
+
+          if (idx > -1) {
+
+            $scope.volunteers.splice(idx, 1);
+          }
+
+        } else {
+
+          var idx = -1;
+
+          for (var i = 0; i < $scope.pendingVolunteers.length; i++) {
+
+            if ($scope.pendingVolunteers[i]._id === volunteerId) {
+
+              idx = i;
+
+              break;
+            }
+
+          }
+
+          if (idx > -1) {
+
+            $scope.pendingVolunteers.splice(idx, 1);
+          }
+
+        }
+
+        $window.alert('Volunteer removed!');
+
+      }).error(function(err, status) {
+
+        processError(err, status);
+      });
+    };
+
+    $scope.addTaskSchedule = function() {
+      TaskService.addTaskSchedule($scope.volunteer.taskId, $scope.volunteer._id, $scope.schedule).success(function(res) {
+
+        for (var i = 0; i < $scope.tasks.length; i++) {
+
+          if ($scope.tasks[i]._id === $scope.volunteer.taskId) {
+
+            var idx = -1;
+
+            for (var j = 0; j < $scope.tasks[i].volunteers.length; j++) {
+
+              if ($scope.tasks[i].volunteers[j].volunteer_ref._id === $scope.volunteer._id) {
+
+                $scope.tasks[i].volunteers[j].volunteer_ref.schedules = $scope.tasks[i].volunteers[j].volunteer_ref.schedules || [];
+
+                $scope.tasks[i].volunteers[j].volunteer_ref.schedules.push(angular.copy($scope.schedule));
+                $scope.schedule = {};
+                $scope.volunteer = {};
+                $window.alert('Schedule added');
+                break;
+              }
+            }
+          }
+
+          break;
+        }
+
+      }).error(function(err, status) {
+
+        processError(err, status);
+      });
+    }
+
+    $scope.deleteSchedule = function(taskId, volunteerId, scheduleId) {
+      TaskService.deleteSchedule(volunteerId, scheduleId).success(function(res) {
+
+        for (var i = 0; i < $scope.tasks.length; i++) {
+
+          if ($scope.tasks[i]._id === taskId) {
+
+            var idx = -1;
+
+            for (var j = 0; j < $scope.tasks[i].volunteers.length; j++) {
+
+              if ($scope.tasks[i].volunteers[j].volunteer_ref._id === volunteerId) {
+
+                var idx = -1;
+                for (var k = 0; k < $scope.tasks[i].volunteers[j].volunteer_ref.schedules.length; k++) {
+
+                  if ($scope.tasks[i].volunteers[j].volunteer_ref.schedules[k]._id === scheduleId) {
+
+                    idx = k;
+                    break;
+                  }
+                }
+
+                if (idx > -1) {
+
+                  $scope.tasks[i].volunteers[j].volunteer_ref.schedules.splice(idx, 1);
+                  $window.alert('Schedule deleted');
+                }
+                break;
+              }
+            }
+          }
+
+          break;
+        }
+
+      }).error(function(err, status) {
+
+        processError(err, status);
+      });
+    };
+
+    $scope.editSchedule = function(volunteerId, scheduleId, schedule) {
+      TaskService.editSchedule(volunteerId, scheduleId, schedule).success(function(res) {
+
+        $window.alert('Schedule modified.');
+
+      }).error(function(err, status) {
+
+        processError(err, status);
+      });
     };
 
     function processError(err, status) {
@@ -167,6 +417,41 @@ angular.module('eventApp')
 
         return (user.firstname.indexOf(searchText) === 0 || user.lastname.indexOf(searchText) === 0 || user.email.indexOf(searchText) === 0);
       });
+    };
+
+    $scope.selectedUserChange = function(volunteer) {
+      $scope.volunteer = angular.extend({}, volunteer);
+
+    };
+
+
+    $scope.querySearch = function(searchText) {
+
+      if (!searchText || searchText === "") {
+        return $scope.volunteers;
+      }
+      searchText = angular.lowercase(searchText);
+
+      return $scope.volunteers.filter(function(volunteer) {
+
+        return (volunteer.user_ref.firstname.indexOf(searchText) === 0 || volunteer.user_ref.lastname.indexOf(searchText) === 0 || volunteer.user_ref.email.indexOf(searchText) === 0);
+      });
+    };
+
+    $scope.searchLabel = function() {
+      return $scope.volunteers.length ? "Select volunteer to assign task" : "No volunteer found";
+    };
+
+    $scope.isEventOwner = function() {
+      return $scope.role === "owner";
+    };
+
+    $scope.isTaskManager = function() {
+      return $scope.role === "manager";
+    };
+
+    $scope.isVolunteer = function() {
+      return $scope.role === "volunteer";
     };
 
     function parseDate(date) {
--- public/app/services/task.service.js
+++ public/app/services/task.service.js
@@ -23,6 +23,50 @@ angular.module('eventApp')
       deleteTask: function(eventId, taskId) {
         var token = localStorage.getItem('userToken');
         return $http.delete("/api/event/" + eventId + "/task/" + taskId + "?token=" + token);
+      },
+      getAllUserTasks: function() {
+        var token = localStorage.getItem('userToken');
+        return $http.get("/api/user/tasks?token=" + token);
+      },
+      getVolunteeredTasks: function() {
+        var token = localStorage.getItem('userToken');
+        return $http.get("/api/user/volunteers?token=" + token);
+      },
+      getAllUserEventTasks: function(eventId) {
+        var token = localStorage.getItem('userToken');
+        return $http.get("/api/event/" + eventId + "/user/tasks?token=" + token);
+      },
+      getAllMyEventVolunteers: function(eventId) {
+        var token = localStorage.getItem('userToken');
+        return $http.get("/api/event/" + eventId + "/user/volunteers?token=" + token);
+      },
+      getEventPendingVolunteers: function(eventId) {
+        var token = localStorage.getItem('userToken');
+        return $http.get("/api/event/" + eventId + "/volunteers/pending?token=" + token);
+      },
+      addVolunteerToTask: function(taskId, volunteerId) {
+        var token = localStorage.getItem('userToken');
+        return $http.post("/api/task/" + taskId + "/volunteers/" + volunteerId + "?token=" + token);
+      },
+      removeVolunteerFromTask: function(taskId, volunteerId) {
+        var token = localStorage.getItem('userToken');
+        return $http.delete("/api/task/" + taskId + "/volunteers/" + volunteerId + "?token=" + token);
+      },
+      addTaskSchedule: function(taskId, volunteerId, schedule) {
+        var token = localStorage.getItem('userToken');
+        return $http.post("/api/task/" + taskId + "/volunteers/" + volunteerId + "/schedules?token=" + token, {
+          schedule: schedule
+        });
+      },
+      editSchedule: function(volunteerId, scheduleId, schedule) {
+        var token = localStorage.getItem('userToken');
+        return $http.put("/api/task/volunteers/" + volunteerId + "/schedules/" + scheduleId + "?token=" + token, {
+          schedule: schedule
+        });
+      },
+      deleteSchedule: function(volunteerId, scheduleId) {
+        var token = localStorage.getItem('userToken');
+        return $http.delete("/api/task/volunteers/" + volunteerId + "/schedules/" + scheduleId + "?token=" + token);
       }
     };
   }]);
